d02ea330952e9d1c43c9cfc2f73385352349de29
Homura-san <erickbispo@ufba.br>
2023-08-26 12:32:39 -0300

    foreach

--- Fundamentos/repeticao/for.php
+++ Fundamentos/repeticao/for.php
@@ -2,7 +2,13 @@
 
 $lista = ['Primeiro', 'Segundo', 3, 'False', TRUE];
 
-for ($i = 0; $i < count($lista); $i++){
-    echo "{$lista[$i]}";
-    echo " ";
+#for ($i = 0; $i < count($lista); $i++){
+// for ($i = count($lista) - 1; $i >= 0; $i--){
+//     echo "{$lista[$i]}";
+//     echo " ";
+// }
+
+foreach ($lista as $key) {
+    echo "O valor atual é: {$key}.
+    ";
 }
--- aplicacao de teste/index.php
+++ aplicacao de teste/index.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de compras</title>
+</head>
+<body>
+    <?php $lista = ['Arroz', 'Carne', 'cebola', 'açafrão']; ?>
+    <h1>Lista de Compras</h1>
 
-<html>
-
-    <body>
-        <?php 
-            $link = "Segunda Página";
-        ?>
-
-        <p>
-            Link: <a href="pg2.php?id=1&name=Erick"><?php echo $link; ?></a>
-        </p>
+    <ul>
+        <?php foreach ($lista as $i):?>
+            <li> <?= $i ?> </li>
+        <?php endforeach ?>
         
-    </body>
-
+    </ul>
+</body>
 </html>
+
+<?php # Como iniciar servidor: php -S localhost:8000 ?>
