9aa926931024e9486c19dc5a2c568d61086e5604
Homura-san <erickbispo@ufba.br>
2023-08-29 16:17:18 -0300

    Site exemplo

---
+++ estudo de caso/index.php
@@ -0,0 +1,9 @@
+<?php
+
+# Cabeçalho de metadata
+include './layout/head.php';
+include './layout/header.php';
+include './layout/main.php';
+include './layout/footer.php';
+
+include './layout/scripts.php';
